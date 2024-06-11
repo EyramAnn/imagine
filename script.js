46d987ed62b73dd8ba73e27edf21f382c7ac6ecf
EyramAnn <anndekonor@gmail.com>
2024-06-11 11:50:58 +0000

    Array Methods in JavaScript

--- script.js
+++ script.js
@@ -222,3 +222,87 @@ alphabets.join(' ').split(' ');
 
 
 
+// Write a function that will take firstname and lastname and return fullname;
+function fullname() {
+    return 'Antoinette Amedza'
+  }
+  fullname();
+  
+  
+  
+  
+  // Write a function that will take firstname and lastname and return fullname;
+  function fullName (firstName, Lastname) {
+    return `${firstName} ${Lastname}`;
+  }
+  fullName('Antoinette', 'Amedza');
+  
+  
+  
+  
+  // Write a function that will take a user with firstname and lastname and return fullname;
+  const user = {
+    firstname: 'Antoinette',
+    lastname: 'Amedza'
+  }
+  function fullname(user){
+    return `${firstname} ${lastname}`;
+  }
+  fullname(user);
+  
+  
+  // Write a function that will take a user with firstname and lastname and return fullname;
+  const user = {
+    firstname: 'Antoinette',
+    lastname: 'Amedza'
+  }
+  function fullname(user){
+    return {
+      ...user,
+      fullname:`${user.firstname} ${user.lastname}`
+    }
+  }
+  fullname(user);
+  
+  
+  
+  
+  
+  
+  
+  
+  // Array map
+  const users = [
+    {firstname: 'Linda', lastname: 'Parker'},
+    {firstname: 'Mark', lastname: 'Twaine'},
+    {firstname: 'Oliver', lastname: 'Queen'},
+    {firstname: 'Stephen', lastname: 'Grider'},
+    {firstname: 'Elon', lastname: 'Musk'},
+  ]
+  users.map(fullname);
+  
+  
+  // Map and return square of numbers
+  function square(number) {
+    return number ** 2;
+  }
+  const numbers =[0,1,2,3,4,5]
+  numbers.map(square);
+  
+  
+  // Filter and return even numbers
+  function even(number) {
+    return number % 2 === 0;
+  }
+  numbers.filter(even);
+  
+  
+  // Filter and return odd numbers
+  function odd(number) {
+    return number % 2 === 1;
+  }
+  numbers.filter(odd);
+  
+  
+  
+
